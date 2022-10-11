fbd7891022955f0c066253cf8493793187650573
Silia Cronauer <silia.maria@web.de>
2022-10-11 13:44:46 +0200

    homepage

--- carRental/src/app/home/home.component.ts
+++ carRental/src/app/home/home.component.ts
@@ -6,6 +6,63 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  cars: Array<{name: string, price: number, seats: number, transmission: string, img: string}> = [{
+name: "OpelCorsa",
+price: 100,
+seats: 5,
+transmission: "manual",
+img: "/assets/images/opel.jpg"
+},
+{
+name: "Mercedes",
+price: 300,
+seats: 4,
+transmission: "automatic",
+img: "/assets/images/mercedes.jpg"
+},
+{
+name: "VW-Bus",
+price: 300,
+seats: 8,
+transmission: "automatic",
+img: "/assets/images/vwbus.jpg" 
+},
+{
+name: "Ford",
+price: 150,
+seats: 3,
+transmission: "manual",
+img: "/assets/images/ford.jpg"
+},
+{
+name: "Fiat",
+price: 100,
+seats: 4,
+transmission: "manual",
+img: "/assets/images/fiat.jpg"
+},
+{
+name: "Smart",
+price: 90,
+seats: 2,
+transmission: "automatic",
+img: "/assets/images/smart.jpg"
+},
+{
+name: "BMW",
+price: 250,
+seats: 5,
+transmission: "automatic",
+img: "/assets/images/bmw.jpg"
+},
+{
+name: "Honda",
+price: 150,
+seats: 5,
+transmission: "manual",
+img: "/assets/images/honda.jpg"
+}
+];
 
   constructor() { }
 
