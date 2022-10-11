0bf242584383ab7ba6c658e8206279e2c4a40c3f
Silia Cronauer <silia.maria@web.de>
2022-10-11 16:40:13 +0200

    form in contact page added

--- carRental/src/app/app.module.ts
+++ carRental/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HeroComponent } from './hero/hero.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { FooterComponent } from './footer/footer.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { FooterComponent } from './footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- carRental/src/app/contact/contact.component.ts
+++ carRental/src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -6,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+  info = new FormGroup ({
+    email: new FormControl ("",Validators.email),
+     title: new FormControl ("",Validators.maxLength(3)),
+     firstName: new FormControl ("",Validators.required),
+    lastName: new FormControl ("",Validators.required),
+    password: new FormControl("",Validators.required),
+    number: new FormControl(""),
+   country: new FormControl("",Validators.required),
+    cardName: new FormControl("",Validators.required),
+    cardNumber: new FormControl("",Validators.required),
+    expDate: new FormControl("",Validators.required),
+    cvc: new FormControl("",Validators.required),      
+  })
 
   constructor() { }
 
